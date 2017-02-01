e919efe82dfb745a39059eba7747a43161c908af
Jason Petterson <jpetterson@detroitlabs.com>
2017-02-01 13:35:02 -0500

    wrapped problems in classes to avoid conflicts between logic

--- src/Day1.kt
+++ src/Day1.kt
@@ -2,89 +2,92 @@ import java.io.BufferedReader
 import java.io.FileReader
 import java.util.regex.Pattern
 
-enum class Direction {
-    NORTH, SOUTH, WEST, EAST
-}
-
-data class Instruction(val turn: String, val distance: Int)
-
-data class Point(var x: Int, var y: Int)
-
-fun readInput() : List<Instruction> {
-    val instructionList = mutableListOf<Instruction>()
-    val reader : BufferedReader = BufferedReader(FileReader("day1_input.txt"))
-
-    for (line in reader.readLines()) {
-        instructionList.addAll(parseLine(line))
-        parseLine (line)
+class Day1 {
+    enum class Direction {
+        NORTH, SOUTH, WEST, EAST
     }
 
-    return instructionList
-}
+    data class Instruction(val turn: String, val distance: Int)
 
-fun parseLine(line : String) : List<Instruction> {
-    val instructionList = mutableListOf<Instruction>()
-    val instructionPattern = Pattern.compile("""(\p{Upper})(\d+)""")
+    data class Point(var x: Int, var y: Int)
 
-    line.split(", ").map {
-        val instructionMatcher = instructionPattern.matcher(it)
-        if (instructionMatcher.matches()) {
-            val instruction = Instruction(turn = instructionMatcher.group(1), distance = (Integer.parseInt(instructionMatcher.group(2))))
-            instructionList.add(instruction)
+    fun readInput() : List<Instruction> {
+        val instructionList = mutableListOf<Instruction>()
+        val reader : BufferedReader = BufferedReader(FileReader("day1_input.txt"))
+
+        for (line in reader.readLines()) {
+            instructionList.addAll(parseLine(line))
+            parseLine (line)
         }
+
+        return instructionList
     }
 
-    return instructionList
-}
+    fun parseLine(line : String) : List<Instruction> {
+        val instructionList = mutableListOf<Instruction>()
+        val instructionPattern = Pattern.compile("""(\p{Upper})(\d+)""")
 
-fun calculateDistance() {
-    var currentPoint = Point(0, 0)
-    var currentDirection = Direction.NORTH
-    val instructions = readInput()
-    println("Instructions = " + instructions)
-    instructions.map {
-        currentDirection = turn(currentDirection, it.turn)
-        currentPoint = move(currentDirection, it.distance, currentPoint)
+        line.split(", ").map {
+            val instructionMatcher = instructionPattern.matcher(it)
+            if (instructionMatcher.matches()) {
+                val instruction = Instruction(turn = instructionMatcher.group(1), distance = (Integer.parseInt(instructionMatcher.group(2))))
+                instructionList.add(instruction)
+            }
+        }
+
+        return instructionList
     }
 
-    val distance = Math.abs(currentPoint.x) + Math.abs(currentPoint.y)
-    println("End point = " + currentPoint)
-    println("Distance = " + distance)
-}
+    fun calculateDistance() {
+        var currentPoint = Point(0, 0)
+        var currentDirection = Direction.NORTH
+        val instructions = readInput()
+        println("Instructions = " + instructions)
+        instructions.map {
+            currentDirection = turn(currentDirection, it.turn)
+            currentPoint = move(currentDirection, it.distance, currentPoint)
+        }
 
-fun turn(dir: Direction, turn: String) : Direction {
-    if (turn == "L") {
+        val distance = Math.abs(currentPoint.x) + Math.abs(currentPoint.y)
+        println("End point = " + currentPoint)
+        println("Distance = " + distance)
+    }
+
+    fun turn(dir: Direction, turn: String) : Direction {
+        if (turn == "L") {
+            when (dir) {
+                Direction.NORTH -> return Direction.WEST
+                Direction.SOUTH -> return Direction.EAST
+                Direction.WEST -> return Direction.SOUTH
+                Direction.EAST -> return Direction.NORTH
+            }
+        }
+        else if (turn == "R") {
+            when (dir) {
+                Direction.NORTH -> return Direction.EAST
+                Direction.SOUTH -> return Direction.WEST
+                Direction.WEST -> return Direction.NORTH
+                Direction.EAST -> return Direction.SOUTH
+            }
+        }
+        return dir
+    }
+
+    fun move(dir: Direction, distance: Int, location: Point) : Point {
+        var newPoint = Point(location.x, location.y)
+
         when (dir) {
-            Direction.NORTH -> return Direction.WEST
-            Direction.SOUTH -> return Direction.EAST
-            Direction.WEST -> return Direction.SOUTH
-            Direction.EAST -> return Direction.NORTH
+            Direction.NORTH -> newPoint.y += distance
+            Direction.SOUTH -> newPoint.y -= distance
+            Direction.WEST -> newPoint.x -= distance
+            Direction.EAST -> newPoint.x += distance
         }
+
+        return newPoint
     }
-    else if (turn == "R") {
-        when (dir) {
-            Direction.NORTH -> return Direction.EAST
-            Direction.SOUTH -> return Direction.WEST
-            Direction.WEST -> return Direction.NORTH
-            Direction.EAST -> return Direction.SOUTH
-        }
-    }
-    return dir
 }
 
-fun move(dir: Direction, distance: Int, location: Point) : Point {
-    var newPoint = Point(location.x, location.y)
-
-    when (dir) {
-        Direction.NORTH -> newPoint.y += distance
-        Direction.SOUTH -> newPoint.y -= distance
-        Direction.WEST -> newPoint.x -= distance
-        Direction.EAST -> newPoint.x += distance
-    }
-
-    return newPoint
-}
 
 fun main(args: Array<String>) {
-    calculateDistance()
+    Day1().calculateDistance()
 }
---
+++ src/Day2.kt
@@ -0,0 +1,19 @@
+
+class Day2 {
+    data class Instruction(val dir: String)
+
+    fun readInput() : List<Instruction> {
+        return emptyList()
+    }
+
+    fun calculateBathroomCode() {
+        val instructionList = readInput()
+        var code = ""
+
+        println("code = $code")
+    }
+}
+
+fun main(args: Array<String>) {
+    Day2().calculateBathroomCode()
+}
