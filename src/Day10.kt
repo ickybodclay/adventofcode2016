7f87fb2f4653010720d8a59b8280040b047b5536
Jason Petterson <jpetterson@detroitlabs.com>
2017-03-23 17:46:38 -0400

    added solution for day 10 part 1

--- src/Day10.kt
+++ src/Day10.kt
@@ -6,8 +6,18 @@ class Day10 {
     data class ValueInstruction(val value: Int, val botId: Int)
     data class BotInstruction(val botId: Int, val lowDestination: String, val lowDestinationId: Int, val highDestination: String, val highDestinationId: Int)
 
+    class Bot (botId: Int) {
+        val id = botId
+        val chipList = mutableListOf<Int>()
+        val instructionList = mutableListOf<BotInstruction>()
+    }
+
     val valuePattern: Pattern = Pattern.compile("""value (\d+) goes to bot (\d+)""")
     val botPattern: Pattern = Pattern.compile("""bot (\d+) gives low to (bot|output) (\d+) and high to (bot|output) (\d+)""")
+
+    val botMap = mutableMapOf<Int, Bot>()
+    var checkLow: Int = 0
+    var checkHigh: Int = 0
 
     fun readInput() {
         val reader = BufferedReader(FileReader("day10_input.txt"))
@@ -31,20 +41,84 @@ class Day10 {
                 followInstruction(instruction)
             }
         }
+
+        while (true) {
+            var hasFollowedInstruction = false
+
+            botMap.map {
+                if (it.value.chipList.size >= 2 && it.value.instructionList.size > 0) {
+                    followInstruction(it.value.instructionList.removeAt(0))
+                    hasFollowedInstruction = true
+                }
+            }
+
+            if (!hasFollowedInstruction) break
+        }
     }
 
     fun followInstruction(instruction: Any) {
         if (instruction is ValueInstruction) {
-            println("ValueInstruction>\t" + instruction)
+            //println("ValueInstruction>\t" + instruction)
+            if (!botMap.containsKey(instruction.botId)) {
+                botMap[instruction.botId] = Bot(instruction.botId)
+            }
+
+            botMap[instruction.botId]!!.chipList.add(instruction.value)
         }
         else if (instruction is BotInstruction) {
-            println("BotInstruction>\t\t" + instruction)
+            //println("BotInstruction>\t\t" + instruction)
+            if (!botMap.containsKey(instruction.botId)) {
+                botMap[instruction.botId] = Bot(instruction.botId)
+            }
+
+            val bot = botMap[instruction.botId]
+            if (bot!!.chipList.size >= 2) {
+                val botInstruction: BotInstruction
+
+                if (bot.instructionList.size > 0) {
+                    botInstruction = bot.instructionList.removeAt(0)
+                    bot.instructionList.add(instruction)
+                }
+                else {
+                    botInstruction = instruction
+                }
+
+                bot.chipList.sort()
+                val low = bot.chipList.first()
+                val high = bot.chipList.last()
+
+                if (checkLow == low && checkHigh == high) {
+                    println("bot ${bot.id} checking low $low and high $high")
+                }
+
+                if (botInstruction.lowDestination == "bot") {
+                    if (!botMap.containsKey(botInstruction.lowDestinationId)) {
+                        botMap[botInstruction.lowDestinationId] = Bot(botInstruction.lowDestinationId)
+                    }
+
+                    bot.chipList.remove(low)
+                    botMap[botInstruction.lowDestinationId]!!.chipList.add(low)
+                }
+
+                if (botInstruction.highDestination == "bot") {
+                    if (!botMap.containsKey(botInstruction.highDestinationId)) {
+                        botMap[botInstruction.highDestinationId] = Bot(botInstruction.highDestinationId)
+                    }
+
+                    bot.chipList.remove(high)
+                    botMap[botInstruction.highDestinationId]!!.chipList.add(high)
+                }
+            }
+            else {
+                botMap[instruction.botId]!!.instructionList.add(instruction)
+            }
         }
     }
 
     fun printBotNumberResponsibleForComparing(low: Int, high: Int) {
+        checkLow = low
+        checkHigh = high
         readInput()
-
     }
 }
 
